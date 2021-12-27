68316832b28a0576f3d9092268f2fac35055841f
ryanapp <1679099+ryanapp@users.noreply.github.com>
2021-12-27 15:43:51 +0000

    fiddle with hd config

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
-wget https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
-mkdir virtio-drivers
-( cd virtio-drivers ; bsdtar xfp ../virtio-win.iso )
+#wget https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
+#mkdir virtio-drivers
+#( cd virtio-drivers ; bsdtar xfp ../virtio-win.iso )
+md5=($(md5sum $1))
+packer build -var "iso_path=$1" -var "iso_md5=$md5" packer.json
